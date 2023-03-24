4ec5149aab22034b5e0417660cebc66765a16ba6
EzatMatin <ezatwakily1377@gmail.com>
2023-03-24 16:17:08 -0300

    gerdfghj

--- src/components/Autoras.js
+++ src/components/Autoras.js
@@ -28,7 +28,10 @@ function Autoras() {
         <img src={Rectangle8} className=' mr-[150px] w-[50px] lg:w-[80px]' />
         <h1 className=' text-white text-2xl autoras lg:text-3xl'>AUTORAS</h1>
         <img src={Rectangle9} className='ml-[150px] w-[50px] lg:w-[80px]' />
-        <p className='text-center text-[14px] text-white mx-[60px] mt-3 lg:text-left w-[70%] lg:w-[60%]'></p>
+        <p className='text-center text-[14px] text-white mx-[60px] mt-3 lg:text-left w-[70%] lg:w-[60%]'>Contar histórias é provavelmente 
+        a mais poderosa ferramenta pedagógica. Contar para os outros as histórias do que aconteceu com essas mulheres ref
+        ugiadas  e como elas se integraram em um novo país, no caso o Brasil, pode provocar nas pessoas um sentimento de so
+        lidariedade e de entendimento da importância de receber bem os refugiados.</p>
       </div>
       <div className='flex flex-row space-x-3 justify-between items-center mt-8 big-containe'>
         <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className='containe w-screen lg:w-full md:w-full '>
